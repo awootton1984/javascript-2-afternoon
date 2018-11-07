afa780749512c41ff05def1199cd435e073d7f6a
Adrienne Wootton <adrienne@Owners-MacBook-Pro.local>
2018-11-06 22:39:04 -0700

    callbacks completed day 2

--- practice.js
+++ practice.js
@@ -15,8 +15,9 @@
 */
 
 let me = {
-  name: 'Adrienne', age: '34'
-}
+  name: 'Adrienne', age: '34'}
+//
+// alert(me.name)
 
 
 ////////// PROBLEM 2 //////////
@@ -36,7 +37,8 @@ let favoriteThings = {
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = '4Runner';
+favoriteThings.brand = 'Toyota';
 
 
 
@@ -44,7 +46,8 @@ let favoriteThings = {
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 
@@ -58,7 +61,9 @@ let favoriteThings = {
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {};
+var item = "firstPocket"
+backPack[item] = 'chapstick'
 
 
 
@@ -66,7 +71,7 @@ let favoriteThings = {
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack)
 
 
 
@@ -76,7 +81,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack)
 
 
 
@@ -99,7 +104,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 
 
